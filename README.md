2bce5068db56020c53dae94a6b7e37478339e041
Ian Woloschin <ian@woloschin.com>
2018-02-11 21:46:52 -0500

    Initial commit #2

---
+++ README.md
@@ -0,0 +1,2 @@
+# Language Elvish
+A simple Atom language package to provide syntax highlighting for the [Elvish Shell](https://elvish.io/).
---
+++ spec/language-elvish-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageElvishView from '../lib/language-elvish-view';
+
+describe('LanguageElvishView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
