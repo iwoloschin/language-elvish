817890854c781c3a8427ff8cc4d0f7717088102b
Ian Woloschin <ian.woloschin@akamai.com>
2018-02-16 20:51:41 -0500

    Add the 'all' builtin

--- spec/language-elvish-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageElvishView from '../lib/language-elvish-view';
-
-describe('LanguageElvishView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
